c2fae186f02b7e0eab3c524402ae524a9faefd45
Antonio Sousa <a.j.moreira.sousa@gmail.com>
2019-06-07 16:47:19 +0100

    add: tests to async function

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -93,27 +93,47 @@ $(function() {
         });
     })
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* A new test suite named "Initial Entries" */
     describe('Initial Entries', function() {
-        /* TODO: Write a test that ensures when the loadFeed
+        /* A test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-        it('should show at least a single element', function() {
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+        });
 
+        it('should show at least a single element', function(done) {
+            expect($('.feed .entry').length).toBeGreaterThan(0);
+            done();
         });
     })
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    /* A test suite named "New Feed Selection" */
     describe('New Feed Selection', function() {
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /* A test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-        it('load should change content', function() {
+        let firstFeed, newFeed;
 
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                firstFeed = $('.feed').html();
+                loadFeed(1, function() {
+                    newFeed = $('.feed').html();
+                    done();
+                });
+            });
+        });
+
+        it('load should change content', function(done) {
+            expect(newFeed).not.toEqual(firstFeed);
+            done();
         });
     })
 }());
